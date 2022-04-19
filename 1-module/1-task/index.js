b49c0aa7164d9f54061de4033072e43dacc1bf71
Egor <egorfedorov@MacBook-Air-Egor.local>
2022-04-19 03:11:52 +0300

    Сделал второе задание

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  let i = 1;
+  if (n === 0) {
+    return 1;
+  }
+  while (i <= n) {
+    result = result * i;
+    i++;
+  }
+  return result;
 }
